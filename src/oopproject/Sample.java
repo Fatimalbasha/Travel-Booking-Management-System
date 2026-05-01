5eb8d80906b08dfcbf8467959e9fd37b68fd72e1
Fatimah Albasha <fatimah.albasha2@gmail.com>
2026-05-01 16:29:00 +0300

    Create Sample.java for travel booking management
    Added a new Java class for the Travel Booking Management System.

---
+++ src/oopproject/Sample.java
@@ -0,0 +1,14 @@
+package oopproject;
+
+/**
+ * Travel Booking Management System
+ * A comprehensive Java desktop application for travel booking management
+ * 
+ * @author Your Name
+ */
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Travel Booking Management System");
+        System.out.println("Download complete project from Google Drive");
+    }
+}
